a5081645fefa020fdb306bef429d3c6943cd483d
shannone <shannone426@gmail.com>
2022-08-14 18:37:41 -0400

    addEventListener

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+    // const input = document.getElementById('button');
+
+    // function clickAlert() {
+    //   alert('I was clicked!');
+    // }
+    
+    // input.addEventListener('click', clickAlert);
 }
